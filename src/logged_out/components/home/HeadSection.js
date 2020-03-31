3896bb86fd11d7ab26b99db6ed575043f2500b88
savana-s <savanasathre@gmail.com>
2020-03-30 23:38:44 -0700

    Update HeadSection.js

--- src/logged_out/components/home/HeadSection.js
+++ src/logged_out/components/home/HeadSection.js
@@ -143,7 +143,7 @@ function HeadSection(props) {
                           fullWidth
                           className={classes.extraLargeButton}
                           classes={{ label: classes.extraLargeButtonLabel }}
-                          href="https://github.com/dunky11/react-saas-template"
+                          href="https://github.com/savana-s/saas-commerce"
                         >
                           Download from GitHub
                         </Button>
